671aa7a6fde7e5eb3ba0178b638faf7af990b070
aslin-r <aslinjoelr123@gmail.com>
2023-08-12 16:28:49 +0530

    tried some more operator

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ console.log(true && false) // false
 
 console.log(true || false) // true
 console.log(false || false) // false
+
+console.log(22 % 5); // 2
+console.log(1+'5'); // 15 coercion
+
+var longString = "";
+longString += "Once";
+longString += " upon";
+longString += " a";
+longString += " time";
+longString += "...";
+console.log(longString); // "Once upon a time..."
