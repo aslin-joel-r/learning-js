62357acd9be301093c694ec574b3ffe227eaf9d0
aslin-r <aslinjoelr123@gmail.com>
2023-08-15 20:51:21 +0530

    if statements without elseif

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
-console.log(true && true) // true
-console.log(true && false) // false
+// Trying the condition statements
 
-console.log(true || false) // true
-console.log(false || false) // false
+// if and else conditions 
 
-console.log(22 % 5); // 2
-console.log(1+'5'); // 15 coercion
+var mark=40;
+var grade='';
 
-var longString = "";
-longString += "Once";
-longString += " upon";
-longString += " a";
-longString += " time";
-longString += "...";
-console.log(longString); // "Once upon a time..."
+if (mark>35){
+    grade='D';
+    console.log('Your grade is :',grade,"\nYou have just passed..!")
+}
+if (mark>60){
+    grade='C';
+    console.log('Your grade is :',grade,"\nTry More..!")
+}
+if (mark>70){
+    grade='B';
+    console.log('Your grade is :',grade,"\nGood..!")
+}
+if (mark>80){
+    grade='A';
+    console.log('Your grade is :',grade,"\nVery Good..!")
+}
+if (mark>90){
+    grade='O';
+    console.log('Your grade is :',grade,"\nOutstanding Performance..!")
+}
+else{
+    grade='F';
+    console.log('Your grade is :',grade,"\nYou have failed..!")
+}
