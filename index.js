2dad56366d7a5721a4ea7854cf711b5a6e58af69
aslin-r <aslinjoelr123@gmail.com>
2023-08-29 19:56:45 +0530

    tried the array build in methods

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-var car ={
-    model:'Rolls Royce',
-    cost:200000,
-    tax:3000,
-    legal:true,
-    color:'red'
+var arr=[]
+
+function collector(a,b,c,d){
+    arr.push(a);
+    arr.push(b);
+    arr.push(c);
+    arr.push(d);
+    arr.pop();
+    
+    return arr
 }
-car.seats=6;
-car['speed']=500;
-car['no of doors']=4;
-car['2021']=2023;
-console.log(car)
+
+console.log(collector('aslin','joel','r','cse15'))
