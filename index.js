50c75a2ecb6b1e916f4ada2df615b2949f3b661e
aslin-r <aslinjoelr123@gmail.com>
2023-09-14 20:59:33 +0530

    tried some errors

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-var a='helloworldwelcometojavascriptprogramming';
-for (let i=0;i<a.length;i++){
-    console.log(a[i])
-}
+// Bug
+var a=5;
+var b='hello';
 
-console.log()
+var c=a+b
+console.log(c)
+
+// Syntax error
+
+var d='error;
+
+// Reference error
+
+console.log(d);
+
+// Type error
+
+(5).pop;
