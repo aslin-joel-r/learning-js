9ac4f5f504815939fbd106afbe0a0876f67aaf33
aslin-r <aslinjoelr123@gmail.com>
2023-09-18 20:42:21 +0530

    using the oop concepts

--- index.js
+++ index.js
@@ -1,36 +1,35 @@
-// Task 1: Build a function-based console log message generator
-function consoleStyler(color, background, fontSize, txt) {
-    var message = "%c" + txt;
-    var style = `color: ${color};`
-    style += `background: ${background};`
-    style += `font-size: ${fontSize};`
-    console.log(message, style)
+// using the classes
+
+class Animals{
+   
+    constructor(name,types){
+      this.name=name;
+      this.type=types;
+    }
+    hunts(){
+        console.log(`I like hunting `);
+    }
+    living(){
+        console.log(`${this.name}  lives in ${this.type}`);
+    }
 }
 
-// Task 2: Build another console log message generator
-function celebrateStyler(reason) {
-    var fontStyle = "color: tomato; font-size: 50px"
-    if (reason == "birthday") {
-        console.log(`%cHappy birthday`, fontStyle)
-    } else if (reason == "champions") {
-        console.log(`%cCongrats on the title!`, fontStyle)
-    } else {
-        console.log(message, style)
+let lion=new Animals("lion",'land');
+console.log(lion.name);
+lion.hunts();
+lion.living();
+
+// Using the inheritance 
+
+class Birds extends Animals{
+
+    constructor (name,type){
+        super(name,type);
     }
     
 }
 
-// Task 3: Run both the consoleStyler and the celebrateStyler functions
-consoleStyler('#1d5c63', '#ede6db', '40px', 'Congrats!')
-celebrateStyler('birthday')
+var pigeon = new Birds("pigeon",'trees');
 
-
-// Task 4: Insert a congratulatory and custom message
-function styleAndCelebrate(color, background, fontSize, txt, reason) {
-    consoleStyler(color, background, fontSize, txt)  
-    celebrateStyler(reason)
-    
-}
-// Call styleAndCelebrate
-
-styleAndCelebrate('#1d5c63', '#ede6db', '40px', 'Congrats!', 'champions')
+pigeon.living();
+console.log("done")
