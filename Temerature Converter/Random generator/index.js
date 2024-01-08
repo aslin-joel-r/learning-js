8192331973368cb1328996973c8754acb0572ebb
aslin-r <aslinjoelr123@gmail.com>
2024-01-08 22:50:48 +0530

    created the temperature converter

---
+++ Temerature Converter/Random generator/index.js
@@ -0,0 +1,21 @@
+let type;
+let temp;
+
+let display=document.getElementById("display")
+
+function tempCalculator(){
+
+    let f=document.getElementById('kel');
+    let fahrenheit,celcius;
+    temp=document.getElementById("inp").value;
+    temp=Number(temp);
+
+    if(f.checked){
+        celcius=(temp-32)*5/9;
+        display.textContent=`${celcius} ° C`;
+    }
+    else{
+        fahrenheit=temp*(9/5)+32
+        display.textContent=`${fahrenheit} ° F`
+    }
+}
