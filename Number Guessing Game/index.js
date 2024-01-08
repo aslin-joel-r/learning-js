073fc425e5dde3d63932436901a45b18552fe03b
aslin-r <aslinjoelr123@gmail.com>
2024-01-08 20:10:42 +0530

    super

--- Number Guessing Game/index.js
+++ Number Guessing Game/index.js
@@ -1,26 +1,45 @@
 let num=Math.round(Math.random()*100)
 let guess;
-let d;
+let d; // for displaying the info of input
+let dis;
+
 function submit(){
     guess=document.getElementById("input").value;
-    if(guess==num){
-        d=document.getElementById("tip")
-        d.textContent="Your Guess is correct , You win the Match";
+    dis=document.getElementById('display');
+    d=document.getElementById("tip");
+    
+    if(isNaN(guess)){
+        dis.textContent=guess;
+        d.textContent=`Enter a Valid Number`;
+        d.style.color='red';
+        dis.style.color='red';
+    }
+    else{
+        dis.textContent=guess;
+    
+        if(guess==num){
+       
+        d.textContent=` is correct , You win the Match`;
+        dis.style.color='green';
         d.style.color='green';
     }
 
     else if(guess <num){
-        d=document.getElementById("tip")
-        d.textContent="Your Guess is too low"
-        d.style.color='red'
+        d=document.getElementById("tip");
+        d.textContent=`is too low`;
+        d.style.color='purple';
+        dis.style.color='purple';
     }
     else{
-        d=document.getElementById("tip")
-        d.textContent="Your Guess is too high"
-        d.style.color='red'
+        d=document.getElementById("tip");
+        d.textContent=`is too high`;
+        d.style.color='purple';
+        dis.style.color='purple';
+    }
     }
    
 
 }
+// For checking the results correct or wrong
 console.log(num)
 console.log(guess)
