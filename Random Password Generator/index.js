a3e72b0862c7e66265852b9f26acbca1384e7b7e
aslin-r <aslinjoelr123@gmail.com>
2024-01-09 22:23:01 +0530

    project doned password generator

--- Random Password Generator/index.js
+++ Random Password Generator/index.js
@@ -1,24 +1,47 @@
 let password=[];
-let isLower=true;
-let isUpper=true;
-let isSymbols=true;
-let length=25;
+let isLower;
+let isUpper;
+let isSymbols;
+let length;
+let char=[];
 
 function passwordGenerator(){
-    // const lower="abcdefghijklmnopqrstuvwxyz"
-    // const upper ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    // const special="!@#$%^&*()-=+{}[];_:'\"|\\,./?`~<>"
-    const char="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()-=+{}[];_:'\"|\\,./?`~<>"
-    // console.log(lower)
-    // console.log(upper)
-    console.log(char.length)
+    password=[];
+    char=[]
+
+    const lower="abcdefghijklmnopqrstuvwxyz"
+    const upper ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const special="!@#$%^&*()-=+{}[];_:'\"|\\,./?`~<>"
+   
+    isLower=document.getElementById('lower');
+    isUpper=document.getElementById('upper');
+    isSymbols=document.getElementById('symbols');
+   
+    let dis=document.getElementById('display')
+    
+    isLower.checked?char.push(lower):'';
+    isUpper.checked?char.push(upper):'';
+    isSymbols.checked?char.push(special):'';
+    
+    length = document.getElementById('pass-length').value;
+      length = length === '' ? 10 : length;
+
+    char=char.join('');
 
     for(let i=0;i<length;i++){
         let val=Math.floor(Math.random()*char.length)+1;
         password.push(char[val])
     }
     password=password.join('')
-    console.log(password)
+    dis.textContent=password;
 }
 
-passwordGenerator()
+function copyToClipboard() {
+    var content = document.getElementById("display");
+    var textArea = document.createElement("textarea");
+    textArea.value = content.textContent;
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand("Copy");
+    textArea.remove();
+  }
