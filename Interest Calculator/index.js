78e9c47ad507e86f0690997e332d392234ecc18a
aslin-r <aslinjoelr123@gmail.com>
2024-01-12 15:40:34 +0530

    updated code for inteest calculator

--- Interest Calculator/index.js
+++ Interest Calculator/index.js
@@ -4,21 +4,20 @@ function interestCalulate(){
     let principal=document.getElementById('pa').value;
     let rate=document.getElementById('rate').value;
     let year=document.getElementById('year').value;
-    let result;
     const display=document.getElementById('display')
-
+    let result;
     
-    if(principal>0 && !isNaN(principal) && rate>0 && !isNaN(rate) && year>0 && !isNaN(year)){
+
+    principal=(principal>0 && !isNaN(principal))?principal:0;
+    rate=(rate>0 && !isNaN(rate))?rate:0;
+    year=(year>0 && !isNaN(year))?year:0;
+    
+    
         rate=rate/100;
         result=principal*(1+rate/1)**(1*year);
         console.log(result)
         display.textContent=result.toLocaleString('en-IN',{style:'currency',currency:"INR"})
-    }
-    else{
-        if(principal<0){
-            principal
-        }
-    }
+  
     
 }
 function reset(){
