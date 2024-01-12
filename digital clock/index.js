871e407f60c4fcec86b86da70fed44f62b050823
aslin-r <aslinjoelr123@gmail.com>
2024-01-12 17:02:13 +0530

    added the padstart method

--- digital clock/index.js
+++ digital clock/index.js
@@ -4,11 +4,12 @@ function clock(){
     const clock=document.getElementById('clock');
     const date=new Date();
     let hour=date.getHours()
-    const minutes=date.getMinutes()
-    const seconds=date.getSeconds()
+    const minutes=date.getMinutes().toString().padStart(2,0);
+    const seconds=date.getSeconds().toString().padStart(2,0)
     let meredian='AM';
     if(hour>12){
         hour=hour-12;
+        hour=hour.toString().padStart(2,0)
         meredian='PM'
     }
 
