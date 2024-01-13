de31e749fab6f09459eb61bf6b8b02e5f189d804
aslin-r <aslinjoelr123@gmail.com>
2024-01-13 16:40:25 +0530

    completed the basic functionalities

---
+++ Calculator/index.js
@@ -0,0 +1,53 @@
+const display=document.getElementById('display');
+let t;
+let num1;
+let op;
+function numbers(value){
+
+    display.value+=value;
+}
+
+function operation(symbol){
+    op=symbol
+    console.log(typeof symbol)
+    display.value+=symbol;
+    num1=display.value;
+    console.log(typeof num1)
+    t=num1.length;
+    
+    num1=num1.slice(0,t-1)
+    console.log(t)
+    num1=Number(num1)
+    console.log(num1)
+
+}
+
+function equals(){
+    let num2=display.value;
+    num2=num2.slice(t,)
+    console.log(num2)
+    num2=Number(num2);
+    let ans;
+    
+  
+    if(op=='+'){
+        ans=num1+num2;
+    }
+    else if(op=='-'){
+        ans=num1-num2;
+    }
+    else if(op=='*'){
+        ans=num1*num2;
+    }
+    else if(op=='/'){
+        ans=num1/num2;
+        ans=ans.toFixed(5)
+    }
+    display.value=ans;
+}
+function reset(){
+    display.value=0;
+}
+function point(){
+    display.value+='.'
+}
