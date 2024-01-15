5bf0db9755afeffef643564b962447e4cb662d6f
aslin-r <aslinjoelr123@gmail.com>
2024-01-15 20:45:23 +0530

    wonderful and amazing function by js

--- Calculator/index.js
+++ Calculator/index.js
@@ -17,51 +17,15 @@ function operation(symbol){
     console.log(typeof symbol)
     
     display.value+=symbol;
-    num1=display.value;
-    console.log(typeof num1)
-    t=num1.length;
-    
-    num1=num1.slice(0,t-1)
-    console.log(t)
-    num1=Number(num1)
-    console.log(num1)
 
 }
 
 function equals(){
-    let num2=display.value;
-    num2=num2.slice(t,)
-    console.log(num2)
-    num2=Number(num2);
-    let ans;
-    
-  
-    if(operator=='+'){
-        ans=num1+num2;
-    }
-    else if(operator=='-'){
-        ans=num1-num2;
-    }
-    else if(operator=='*'){
-        ans=num1*num2;
-    }
-    else if(operator=='/'){
-        ans=num1/num2;
-        console.log(typeof ans)
-
-      if(ans%1!=0){ // checking is the number is float or not
-        ans=ans.toFixed(3)
-      }
-      
-    }
-    if(display.value=='0' ){
-        ans=0
-    }
-     if(isNaN(ans)){
-        ans="Math Error"
-    }
-    display.value=ans;
-    console.log(ans)
+    let a = display.value;
+    let calculate = new Function('return ' + a);
+    let result = calculate();
+    display.value=result
+    console.log(result);
 }
 function reset(){
     display.value=0;
@@ -75,3 +39,4 @@ function del(){
     dis=dis.slice(0,len-1);
     display.value=dis;
 }
+
