eb64670b52415fe812987a162242ea6be07873fd
aslin-r <aslinjoelr123@gmail.com>
2024-01-16 10:20:09 +0530

    nice working and look

--- Calculator/index.js
+++ Calculator/index.js
@@ -1,7 +1,5 @@
 const display=document.getElementById('display');
-let t;
-let num1;
-let operator;
+
 
 function numbers(value){
     if(display.value=='0'){
@@ -12,20 +10,18 @@ function numbers(value){
    }
 }
 
-
 function equals(){
-    let a = display.value;
-    let calculate = new Function('return ' + a);
-    let result = calculate();
+    const num = display.value;
+    console.log(num)
+    let result = math.evaluate(num);
     display.value=result
     console.log(result);
 }
+
 function reset(){
     display.value=0;
 }
-function point(){
-    display.value+='.'
-}
+
 function del(){
     let dis=display.value;
     let len=dis.length;
