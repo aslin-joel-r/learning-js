03245e563bfba07ab5e51544505af1a6416e3cdf
aslin-r <aslinjoelr123@gmail.com>
2024-01-17 20:10:04 +0530

    works fine

--- image slider/index.js
+++ image slider/index.js
@@ -1,19 +1,35 @@
-const images=document.querySelectorAll('img');
+const images=document.querySelectorAll('.slide');
 const previous=document.getElementById('prev');
 const next=document.getElementById('next');
 
-let slideCount=0;
+let slideIndex=0;
 console.log(images)
-images[slideCount].style.display='block'
+ images[slideIndex].style.display='block'
 
-console.log(slideCount)
+ //setInterval(nextItem,3000)
+
 function previousItem(){
-    slideCount-=1;
-    console.log(slideCount)
-    images[slideCount].style.display='block'
+    
+    images[slideIndex].style.display='none'
+    console.log('Before process:',slideIndex)
+    slideIndex--;
+    if(slideIndex<0){
+        slideIndex=images.length-1
+    }
+   
+    console.log('slide Index :',slideIndex)
+    //console.log('mod is :',slideIndex%=images.length)
+    images[slideIndex].style.display='block'
 }
 function nextItem(){
-    slideCount++;
-    images[slideCount].style.display='block';
-    console.log(slideCount)
+    console.log('slide Index :',slideIndex)
+    console.log('mod is :',slideIndex%=images.length)
+   
+    images[slideIndex].style.display='none';
+    slideIndex++;
+    if(slideIndex>=images.length){
+        slideIndex%=images.length
+    }
+    images[slideIndex].style.display='block'
 }
+   
