b463b894e93310aae5566db1bc953857ebb57061
aslin-r <aslinjoelr123@gmail.com>
2024-01-17 15:15:29 +0530

    works well

--- Rock-Paper-Scissors/index.js
+++ Rock-Paper-Scissors/index.js
@@ -1,9 +1,9 @@
-const choices=['Rock','Paper','Scissors']
+const choices=['Scissors','Rock','Paper'];
 
 const clicked=document.querySelectorAll('.role');
 const player=document.getElementById('player');
 const computerChoice=document.getElementById('computer');
-let statu=document.getElementById('status');
+let gameStatus=document.getElementById('status');
 let pScore=document.getElementById('pscore');
 let cScore=document.getElementById('cscore');
 
@@ -18,44 +18,43 @@ clicked.forEach(element=>{
     element.addEventListener('click',(event)=>{
         current=event.target.name;
         player.textContent=`Player : ${current}`;
-        console.log(current)
 
-        let ran=Math.round(Math.random() *2);
-        console.log(ran);
-        let computer=choices[ran]
-        console.log('computer')
+        let random=Math.round(Math.random() *2);
+        console.log(random);
+        let computer=choices[random]
         computerChoice.textContent=`Computer : ${computer}`
 
         if(current==computer){
-            statu.textContent=`Status : Draw`;
+            gameStatus.textContent=`Status : Draw`;
         }
        else{
+
         if(current=='Scissors' && computer=='Paper'){
-            statu.textContent=`Status : You Won`;
+            gameStatus.textContent=`Status : You Won`;
             playerScore+=1;
         }
         else if(current=='Rock' && computer=='Scissors'){
-            statu.textContent=`Status : You Won`;
+            gameStatus.textContent=`Status : You Won`;
             playerScore+=1;
         }
         else if(current=='Paper' && computer=='Rock'){
-            statu.textContent=`Status : You Won`;
+            gameStatus.textContent=`Status : You Won`;
             playerScore+=1;
         }
         else if(current=='Paper' && computer=='Scissors'){
-            statu.textContent=`Status : Computer Won`;
+            gameStatus.textContent=`Status : Computer Won`;
             computerScore+=1;
         }
         else if(current=='Rock' && computer=='Paper'){
-            statu.textContent=`Status : Computer Won`;
+            gameStatus.textContent=`Status : Computer Won`;
             computerScore+=1;
         }
         else if(current=='Scissors' && computer=='Rock'){
-            statu.textContent=`Status : Computer Won`;
+            gameStatus.textContent=`Status : Computer Won`;
             computerScore+=1;
         }
        }
-       pScore.textContent=`Player Score : ${playerScore}`;
+       pScore.textContent=`Your  Score : ${playerScore}`;
        cScore.textContent=`Computer Score : ${computerScore}`;
 
     })
