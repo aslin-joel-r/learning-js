82c3424fec5842cc57b01fbefd0f2e2d20f52c0f
aslin-r <aslinjoelr123@gmail.com>
2024-01-23 10:31:06 +0530

    looks good

--- Pokemon/index.js
+++ Pokemon/index.js
@@ -1,13 +1,18 @@
 async function getPoke(){
     const pokemon=document.getElementById('pokemon').value;
     const img=document.getElementById('image');
+    let err=document.getElementById('error');
     try{
         let response=await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
         console.log(response)
 
         if(!response.ok){
+            img.style.display='none';
+            err.textContent=`No pokemon named ${pokemon}`;
+            err.style.color='red';
             throw new Error("Enter the valid Pokemon");
         }
+        err.style.display='none'
         const data=await response.json();
         console.log(data)
         img.style.display='block';
